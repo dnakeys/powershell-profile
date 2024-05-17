cc298ac763fb903da9ab15e5893b3c814d146e8a
dnakeys <123251698+dnakeys@users.noreply.github.com>
2024-05-16 20:49:50 -0500

    s

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -142,7 +142,7 @@ function hb {
         return
     }
     
-    $uri = "http://bin.cyberdeanda.ddns.net/documents"
+    $uri = "http://bin.cyberdeanda.ddns.net/documents/"
     try {
         $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
         $hasteKey = $response.key
